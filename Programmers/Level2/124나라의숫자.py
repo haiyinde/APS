54d289525759b08ee3666e28cbabba5460d6d66d
박해인 <haein.hannah.park@gmail.com>
2022-02-08 23:52:15 +0900

    124나라의숫자

---
+++ Programmers/Level2/124나라의숫자.py
@@ -0,0 +1,9 @@
+def solution(n):
+    if n <= 3:
+        return '124'[n - 1]
+    else:
+        tmp1 = (n - 1) // 3
+        tmp2 = (n - 1) % 3
+        return solution(tmp1) + '124'[tmp2]
+
+    return answer
