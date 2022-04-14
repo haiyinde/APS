9116ccb8ba96123af411d4a602d0399e320d3817
박해인 <haein.hannah.park@gmail.com>
2022-04-15 02:19:37 +0900

    2480

--- BJ/2480.py
+++ BJ/2480.py
@@ -1,11 +1,20 @@
 A, B, C = map(int, input().split())
 
-if A == B and B == C and C == A:
-    print(10000+A*1000)
-elif A != B and B != C and C != A:
-    M = max(A, B, C)
-    print(M*100)
+# if A == B and B == C and C == A:
+#     print(10000+A*1000)
+# elif A != B and B != C and C != A:
+#     M = max(A, B, C)
+#     print(M*100)
+# elif A == B or B == C:
+#     print(1000+B*100)
+# elif A == C:
+#     print(1000+A*100)
+
+if A == B and B == C:
+    print(10000 + (A * 1000))
 elif A == B or B == C:
-    print(1000+B*100)
+    print(1000 + (B * 100))
 elif A == C:
-    print(1000+A*100)
+    print(1000 + (A * 100))
+else:
+    print(max(A, B, C)*100)
