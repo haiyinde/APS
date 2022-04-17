2a56d17a26fc0b12631b0525e846f070a204a71e
박해인 <haein.hannah.park@gmail.com>
2022-04-17 13:36:18 +0900

    1065

---
+++ BJ/1065.py
@@ -0,0 +1,12 @@
+N = int(input())
+result = 0
+for n in range(1, N+1):
+    if n < 100:
+        result += 1
+    else:
+        n = str(n)
+        # 1000의 경우 if에서 false되어 자동으로 pass
+        if int(n[1]) - int(n[0]) == int(n[2]) - int(n[1]):
+            result += 1
+
+print(result)
