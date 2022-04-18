051f65896b7b86d6011772becbfb6585686909d5
박해인 <haein.hannah.park@gmail.com>
2022-04-19 00:55:42 +0900

    5622

--- BJ/5622.py
+++ BJ/5622.py
@@ -1,6 +1,22 @@
 S = list(input())
-numbers = list(range(65, 91, 3))
-dict = {}
+result = 0
 
 for s in S:
-    ord(s)
+    if 65 <= ord(s) <= 67:
+        result += 3
+    elif 68 <= ord(s) <= 70:
+        result += 4
+    elif 71 <= ord(s) <= 73:
+        result += 5
+    elif 74 <= ord(s) <= 76:
+        result += 6
+    elif 77 <= ord(s) <= 79:
+        result += 7
+    elif 80 <= ord(s) <= 83:
+        result += 8
+    elif 84 <= ord(s) <= 86:
+        result += 9
+    elif 87 <= ord(s) <= 90:
+        result += 10
+
+print(result)
