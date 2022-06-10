1c1377524bfa1e02ee2cac01ebce69b4f08ba6a9
haiyinde <haein.hannah.park@gmail.com>
2022-06-10 23:20:30 +0900

    10872

--- BJ/10872.py
+++ BJ/10872.py
@@ -2,7 +2,7 @@ def recur_fact(num):
     # num == 1로 하게되면 recur_fact(0)일 때를 받지못해 지속적으로 num-=1이 되어 recursion error 발생
     if num <= 1:
         return 1
-    return num*recur_fact(num-1)
+    return num*recur_fact(num-1) 
 
 N = int(input())
 print(recur_fact(N))
